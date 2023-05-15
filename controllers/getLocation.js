2e99c4cb77da72e0152849054f562f4523944a8f
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-05-15 19:43:20 +0530

    feat: add business logic in controller

---
+++ controllers/getLocation.js
@@ -0,0 +1,60 @@
+import { validator } from "../utils/validateIp.js";
+
+let ip;
+
+//add listener function to the button
+document
+  .getElementsByClassName("searchIp")[0]
+  .childNodes[3].addEventListener("click", async function () {
+
+
+    // //reinitialize map container if already initialized
+    var container = L.DomUtil.get('map');
+    if(container != null){
+        container._leaflet_id = null;
+    }
+
+
+    //get the ip from the input field
+    ip = document.getElementsByClassName("searchIp")[0].childNodes[1].value;
+
+
+    //validate the ip
+    if (!validator(ip)) {
+      alert("Invalid IP");
+      return;
+    }
+
+
+    //send the ip to the geoLocation API to retrieve the location
+    const apiKey = "at_aTvQZ947QB6P2hqWEssL1LAQu6OmP";
+    const location = await fetch(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ip}`
+    );
+    const locationData = await location.json();
+
+
+    //send the location data to the leaflet API
+    const lat = locationData.location.lat;
+    const lng = locationData.location.lng;
+    const mymap = L.map("map");
+    mymap.setView([lat, lng], 13);
+    const attribution = `&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors`;
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const tiles = L.tileLayer(tileUrl, { attribution });
+    tiles.addTo(mymap);
+    const marker = L.marker([lat, lng]).addTo(mymap);
+
+
+    //send the location data to the DOM
+    document.getElementsByClassName("ipAddress").innerHTML =
+      ip;
+    document.getElementsByClassName(
+      "location"
+    ).innerHTML = `${locationData.location.city}, ${locationData.location.country}`;
+    document.getElementsByClassName(
+      "timezone"
+    ).innerHTML = `UTC ${locationData.location.timezone}`;
+    document.getElementsByClassName("isp").innerHTML =
+      locationData.isp;
+});
