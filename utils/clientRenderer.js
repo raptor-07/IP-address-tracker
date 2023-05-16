828ccf785e444f4e7466862b8f4e247dca79bafc
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-05-16 21:12:32 +0530

    fix: redress DOM calls

--- utils/clientRenderer.js
+++ utils/clientRenderer.js
@@ -1,6 +1,8 @@
 export function renderInfoBar(ip, location, timezone, isp){
-    document.getElementsByClassName("ipAddress")[0].innerHTML = ip;
-    document.getElementsByClassName("location")[0].innerHTML = location;
-    document.getElementsByClassName("timezone")[0].innerHTML = timezone;
-    document.getElementsByClassName("isp")[0].innerHTML = isp;
+    // console.log(document.getElementsByClassName("ipAddress")[0]);
+    
+    document.getElementsByClassName("ipAddress")[0].children[1].innerHTML = ip;
+    document.getElementsByClassName("location")[0].children[1].innerHTML = location;
+    document.getElementsByClassName("timezone")[0].children[1].innerHTML = timezone;
+    document.getElementsByClassName("isp")[0].children[1].innerHTML = isp;
 }
