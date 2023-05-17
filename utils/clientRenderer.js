d1b4172d04976885d7fac2036bc7b64d717d59fb
Hurmaan-Ahmed-7 <contactme.hurmaan@gmail.com>
2023-05-17 18:44:05 +0530

    refractor: -

--- utils/clientRenderer.js
+++ utils/clientRenderer.js
@@ -1,5 +1,4 @@
 export function renderInfoBar(ip, location, timezone, isp){
-    // console.log(document.getElementsByClassName("ipAddress")[0]);
     
     document.getElementsByClassName("ipAddress")[0].children[1].innerHTML = ip;
     document.getElementsByClassName("location")[0].children[1].innerHTML = location;
